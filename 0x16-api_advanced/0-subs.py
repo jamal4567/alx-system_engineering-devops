1ea599ec8db64cc76c881e6b49f872d00e70515c
jamal <jamalimmiss97@gmail.com>
2024-08-13 22:27:29 +0000

    advanced pi

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to get the number of subscribers for a given subreddit
+"""
+
+from requests import get
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    
+    # Check if subreddit is valid
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Google Chrome Version 127.0.6533.100'}
+
+    try:
+        response = get(url, headers=user_agent, allow_redirects=False)
+        
+        if response.status_code == 200:
+            results = response.json()
+            return results.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except Exception:
+        return 0
