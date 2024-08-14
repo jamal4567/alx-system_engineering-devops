22757fcb011ec332c8e755bcc7aa5bb1fb8c79f6
jamal <jamalimmiss97@gmail.com>
2024-08-14 20:31:49 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,8 +2,8 @@
 
 $file_path = '/var/www/html/wp-settings.php'
 
-exec {'Fix Error'
+
+exec { 'fix error':
   command => "sed -i 's/phpp/php/g' ${file_path}",
   path    => ['/bin','/usr/bin']
-  provider => shell,
 }
