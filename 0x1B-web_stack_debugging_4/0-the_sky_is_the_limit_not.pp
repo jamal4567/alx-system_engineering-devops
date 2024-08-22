9a977f292f2336468786da081d1f1ab2a01024c8
jamal <jamalimmiss97@gmail.com>
2024-08-22 22:10:21 +0000

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fix amount of traffic for Nginx
+
+exec { 'fix nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart':
+  command => 'sudo service nginx restart',
+  path    => '/etc/init.d/'
+}
